992ab6c8683419a23ef0c3a974cedbec109ed549
Nithin Puthenveettil <nithup123@gmail.com>
2023-05-21 15:02:31 +0530

    bug fix in animatye

--- ui/animate/animate.go
+++ ui/animate/animate.go
@@ -3,13 +3,15 @@ package animate
 import "github.com/nithinputhenveettil/pathfinder-visualiser-golang/visualiser/grid"
 
 func NextTickAnimate(v *grid.Visualiser) {
-	if v.VIndex != len(v.VisitedNodes) {
+	if v.VIndex < len(v.VisitedNodes) {
 		n := v.VisitedNodes[v.VIndex]
 		n.AnimateVisited = true
 		v.VIndex += 1
-	} else if v.SIndex != len(v.ShortPathNodes) {
+	} else if v.SIndex < len(v.ShortPathNodes) {
 		n := v.ShortPathNodes[v.SIndex]
 		n.AnimateShortPath = true
 		v.SIndex += 1
+	} else {
+		return
 	}
 }
